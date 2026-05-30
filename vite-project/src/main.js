e9fff08dce39b9cc1d65cf89fa15439ac1373fd7
AVI-PRA <20104061@apsit.edu.in>
2026-05-30 21:30:47 +0530

    IphoneSe_ResponsiveWithVideoSizeBug

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -11,3 +11,20 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+const videos = [
+    "./src/assets/videos/Elaris-Interior Design Template For Framer (1).mp4",
+    "./src/assets/videos/Elaris-Interior Design Template For Framer (2).mp4",
+    "./src/assets/videos/Elaris-Interior Design Template For Framer (3).mp4"
+];
+
+const player = document.getElementById("videoplayer");
+let current = 0;
+
+player.src = videos[current];
+
+player.addEventListener("ended", () => {
+    current = (current + 1) % videos.length; // loops through array
+    player.src = videos[current];
+    player.play();
+});
